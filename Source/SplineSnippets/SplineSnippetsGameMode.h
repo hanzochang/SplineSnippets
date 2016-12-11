55d61427005edb577978a73c06c52a84e8c9b3e6
hanzochang <hanzo.chan@gmail.com>
2016-12-11 14:59:00 +0900

    Create UE4 proj

---
+++ Source/SplineSnippets/SplineSnippetsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SplineSnippetsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLINESNIPPETS_API ASplineSnippetsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
